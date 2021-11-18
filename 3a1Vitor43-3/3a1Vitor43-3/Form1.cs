68e945836dc6887798734b650dabc738ca4984ba
V-coder-1 <79936970+V-coder-1@users.noreply.github.com>
2021-11-18 01:45:31 -0300

    Add files via upload

---
+++ 3a1Vitor43-3/3a1Vitor43-3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _3a1Vitor43_3
+{
+    public partial class Form1 : Form
+    {
+        Loja objLoja = new Loja();
+        public FrLoja()
+        {
+            InitializeComponent();
+            dtgLoja.DataSource = objCurso.ListarLoja();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            objCurso.nomeFuncionario = txtNomeCurso.Text;
+            objCurso.nomeCargo = txtNomeCargo.Text;
+            objCurso.nomeDescricao = txtNomeDescricao.Text;
+            objCurso.nomeHoras = txtNomeHoras.Int;
+            objCurso.nomeCadastro = txtNomeCadastro.Text;
+
+            objCurso.CadastrarCurso();
+
+            MessageBox.Show("Registro realizado com sucesso!");
+
+            txtNomeCurso.Clear();
+            txtNomeCargo.Clear();
+            txtNomeDescricao.Clear();
+            txtNomeHoras.Clear();
+            txtNomeCadastro.Clear();
+        }
+    }
+}
---
+++ 3a1Vitor43-3/Prova2.sql
@@ -0,0 +1,8 @@
+create database Prova2;
+Use Prova2; 
+create table funcionario(
+id  VARCHAR(20) NOT NULL,
+Produto VARCHAR(40) NOT NULL,
+preco DOUBLE NOT NULL,
+PRIMARY KEY(id)
+)ENGINE=INNODB;
